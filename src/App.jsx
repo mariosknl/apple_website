54b0f6b34d78de9bf399665b8a68b31a05a5d5ce
mariosknl <mariosknl@outlook.com>
2024-03-21 16:51:08 +0200

    eslint & prettier configuration & project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import React from "react";
+import Highlights from "./components/Highlights";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 };
 
 export default App;
