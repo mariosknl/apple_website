a1266b00e281f1859f34927d0cf4253cecbb27ba
mariosknl <mariosknl@outlook.com>
2024-03-22 12:06:24 +0200

    features section & how it works & footer created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Navbar from "./components/Navbar";
 import Model from "./components/Model";
 
 import * as Sentry from "@sentry/react";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 
 const App = () => {
 	return (
@@ -12,6 +15,9 @@ const App = () => {
 			<Hero />
 			<Highlights />
 			<Model />
+			<Features />
+			<HowItWorks />
+			<Footer />
 		</main>
 	);
 };
